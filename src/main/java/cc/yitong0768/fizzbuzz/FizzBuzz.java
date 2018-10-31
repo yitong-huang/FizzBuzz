486f3d79d566985badce89f5412de5b21d157679
HuangYitong <yitong0768@gmail.com>
2018-10-31 21:22:04 +0800

    project init

---
+++ src/main/java/cc/yitong0768/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,12 @@
+package cc.yitong0768.fizzbuzz;
+
+/**
+ * Hello world!
+ *
+ */
+public class FizzBuzz 
+{
+    public static void main( String[] args )
+    {
+    }
+}
---
+++ src/test/java/cc/yitong0768/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,9 @@
+package cc.yitong0768.fizzbuzz;
+
+/**
+ * Unit test for FizzBuzz.
+ */
+public class FizzBuzzTest 
+{
+		
+}
