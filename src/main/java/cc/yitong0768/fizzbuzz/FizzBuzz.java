125546f8a9f1a934d7b8093ee11694f516d5d6a9
HuangYitong <yitong0768@gmail.com>
2018-10-31 23:35:53 +0800

    add FizzBuzz.java and the testcases

--- src/main/java/cc/yitong0768/fizzbuzz/FizzBuzz.java
+++ src/main/java/cc/yitong0768/fizzbuzz/FizzBuzz.java
@@ -1,12 +1,92 @@
 package cc.yitong0768.fizzbuzz;
 
+import java.security.InvalidParameterException;
+
 /**
- * Hello world!
+ *
  *
  */
-public class FizzBuzz 
-{
-    public static void main( String[] args )
-    {
-    }
+public class FizzBuzz {
+
+	public final static int FIZZ_BASE = 3;
+	public final static int BUZZ_BASE = 5;
+	public final static int FROM = 1;
+	public final static int TO = 100;
+
+	/*
+	 * @description Judge if a number is multiples of n, and if isStage2, judge if
+	 * the number has a n in it.
+	 * 
+	 * @param num, should be >= 0.
+	 * 
+	 * @param n, should be > 0
+	 * 
+	 * @param isStage2, true if in stage2
+	 * 
+	 * @return boolean
+	 */
+	public boolean isMultiContainN(int num, int n, boolean isStage2) {
+		if (n <= 0) {
+			throw new InvalidParameterException("n should be > 0");
+		}
+		if (num < 0) {
+			throw new InvalidParameterException("num should be >= 0");
+		}
+		if (num % n == 0) {
+			return true;
+		}
+		if (isStage2) {
+			if (("" + num).contains("" + n)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/*
+	 * @description: print and return the numbers or fizz/buzz in range [from, to].
+	 * 
+	 * @param from, the lower bound, included.
+	 * 
+	 * @param to, the upper bound, included.
+	 * 
+	 * @param isStage2, true if in stage2
+	 * 
+	 * @return
+	 * 
+	 * @see isMultiContainN
+	 */
+	public String printInRange(int from, int to, boolean isStage2) {
+		if (from < 0) {
+			throw new InvalidParameterException("the lower bound should >= 0");
+		}
+		if (to < from) {
+			throw new InvalidParameterException("the upper bound should >= lower bound");
+		}
+		
+		StringBuilder builder = new StringBuilder();
+		for (int i = from; i <= to; i++) {
+			if (isMultiContainN(i, FIZZ_BASE, isStage2)) {
+				if (isMultiContainN(i, BUZZ_BASE, isStage2)) { // both fizz and buzz
+					builder.append("FizzBuzz\n");
+					System.out.println("FizzBuzz");
+				} else { // only fizz
+					builder.append("Fizz\n");
+					System.out.println("Fizz");
+				}
+			} else if (isMultiContainN(i, BUZZ_BASE, isStage2)) { // only buzz
+				builder.append("Buzz\n");
+				System.out.println("Buzz");
+			} else {
+				builder.append(i + "\n");
+				System.out.println(i);
+			}
+		}
+		return builder.toString();
+	}
+
+	public static void main(String[] args) {
+		new FizzBuzz().printInRange(FROM, TO, false);
+	}
 }
--- src/test/java/cc/yitong0768/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/cc/yitong0768/fizzbuzz/FizzBuzzTest.java
@@ -1,9 +1,303 @@
 package cc.yitong0768.fizzbuzz;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.security.InvalidParameterException;
+
+import org.junit.Test;
+
 /**
  * Unit test for FizzBuzz.
  */
-public class FizzBuzzTest 
-{
-		
+public class FizzBuzzTest {
+	private FizzBuzz game = new FizzBuzz();
+
+	@Test
+	public void testIsMultiContainN_stage1_Exception_00() {
+		try {
+			game.isMultiContainN(10, 0, false);
+		} catch (InvalidParameterException e) {
+			return;
+		}
+		fail();
+	}
+
+	@Test
+	public void testIsMultiContainN_stage1_Exception_01() {
+		try {
+			game.isMultiContainN(10, -1, false);
+		} catch (InvalidParameterException e) {
+			return;
+		}
+		fail();
+	}
+
+	@Test
+	public void testIsMultiContainN_stage1_Exception_02() {
+		try {
+			game.isMultiContainN(-1, 5, false);
+		} catch (InvalidParameterException e) {
+			return;
+		}
+		fail();
+	}
+
+	@Test
+	public void testIsMultiContainN_stage1_00() {
+		assertTrue(true == game.isMultiContainN(0, 3, false));
+	}
+
+	@Test
+	public void testIsMultiContainN_stage1_01() {
+		assertTrue(true == game.isMultiContainN(0, 1, false));
+	}
+
+	@Test
+	public void testIsMultiContainN_stage1_02() {
+		assertTrue(true == game.isMultiContainN(1, 1, false));
+	}
+
+	@Test
+	public void testIsMultiContainN_stage1_03() {
+		assertTrue(true == game.isMultiContainN(6, 3, false));
+	}
+
+	@Test
+	public void testIsMultiContainN_stage1_04() {
+		assertTrue(false == game.isMultiContainN(6, 4, false));
+	}
+
+	@Test
+	public void testIsMultiContainN_stage1_05() {
+		assertTrue(true == game.isMultiContainN(1985625425, 397125085, false));
+	}
+	
+	@Test
+	public void testIsMultiContainN_stage1_06() {
+		assertTrue(false == game.isMultiContainN(1985625425, 397125086, false));
+	}
+
+	@Test
+	public void testIsMultiContainN_stage2_Exception_00() {
+		try {
+			game.isMultiContainN(10, 0, true);
+		} catch (InvalidParameterException e) {
+			return;
+		}
+		fail();
+	}
+
+	@Test
+	public void testIsMultiContainN_stage2_Exception_01() {
+		try {
+			game.isMultiContainN(10, -2, true);
+		} catch (InvalidParameterException e) {
+			return;
+		}
+		fail();
+	}
+
+	@Test
+	public void testIsMultiContainN_stage2_Exception_02() {
+		try {
+			game.isMultiContainN(-3, 5, true);
+		} catch (InvalidParameterException e) {
+			return;
+		}
+		fail();
+	}
+
+	@Test
+	public void testIsMultiContainN_stage2_00() {
+		assertTrue(true == game.isMultiContainN(0, 3, true));
+	}
+
+	@Test
+	public void testIsMultiContainN_stage2_01() {
+		assertTrue(true == game.isMultiContainN(0, 1, true));
+	}
+	
+	@Test
+	public void testIsMultiContainN_stage2_02() {
+		assertTrue(true == game.isMultiContainN(1, 1, true));
+	}
+
+	@Test
+	public void testIsMultiContainN_stage2_03() {
+		assertTrue(true == game.isMultiContainN(6, 3, true));
+	}
+
+	@Test
+	public void testIsMultiContainN_stage2_04() {
+		assertTrue(true == game.isMultiContainN(13, 3, true));
+	}
+
+	@Test
+	public void testIsMultiContainN_stage2_05() {
+		assertTrue(false == game.isMultiContainN(14, 3, true));
+	}
+
+	@Test
+	public void testIsMultiContainN_stage2_06() {
+		assertTrue(true == game.isMultiContainN(13456, 34, true));
+	}
+	
+	@Test
+	public void testPrintInRange_Exception_00() {
+		try {
+			game.printInRange(-1, 5, false);
+		} catch (InvalidParameterException e) {
+			return;
+		}
+		fail();
+	}
+	
+	@Test
+	public void testPrintInRange_Exception_01() {
+		try {
+			game.printInRange(-1, -5, false);
+		} catch (InvalidParameterException e) {
+			return;
+		}
+		fail();
+	}
+	
+	@Test
+	public void testPrintInRange_Exception_02() {
+		try {
+			game.printInRange(0, -5, false);
+		} catch (InvalidParameterException e) {
+			return;
+		}
+		fail();
+	}
+	
+	@Test
+	public void testPrintInRange_Exception_03() {
+		try {
+			game.printInRange(5, 4, false);
+		} catch (InvalidParameterException e) {
+			return;
+		}
+		fail();
+	}
+	
+	@Test
+	public void testPrintInRange_stage1_00() {
+		assertTrue(("1\n").equals(game.printInRange(1, 1, false)));
+	}
+	
+	@Test
+	public void testPrintInRange_stage1_01() {
+		assertTrue(("13\n").equals(game.printInRange(13, 13, false)));
+	}
+
+	@Test
+	public void testPrintInRange_stage1_02() {
+		assertTrue(("Fizz\n").equals(game.printInRange(3, 3, false)));
+	}
+
+	@Test
+	public void testPrintInRange_stage1_03() {
+		assertTrue(("Buzz\n").equals(game.printInRange(5, 5, false)));
+	}
+
+	@Test
+	public void testPrintInRange_stage1_04() {
+		assertTrue(("FizzBuzz\n").equals(game.printInRange(15, 15, false)));
+	}
+	
+	@Test
+	public void testPrintInRange_stage1_05() {
+		assertTrue(("1\n" + 
+				"2\n" + 
+				"Fizz\n" + 
+				"4\n" + 
+				"Buzz\n" + 
+				"Fizz\n" + 
+				"7\n" + 
+				"8\n" + 
+				"Fizz\n" + 
+				"Buzz\n" + 
+				"11\n" + 
+				"Fizz\n" + 
+				"13\n" + 
+				"14\n" + 
+				"FizzBuzz\n" + 
+				"16\n").equals(game.printInRange(1, 16, false)));
+	}
+
+	@Test
+	public void testPrintInRange_stage2_00() {
+		assertTrue(("1\n").equals(game.printInRange(1, 1, true)));
+	}
+
+	@Test
+	public void testPrintInRange_stage2_01() {
+		assertTrue(("Fizz\n").equals(game.printInRange(13, 13, true)));
+	}
+
+	@Test
+	public void testPrintInRange_stage2_02() {
+		assertTrue(("Fizz\n").equals(game.printInRange(3, 3, true)));
+	}
+
+	@Test
+	public void testPrintInRange_stage2_03() {
+		assertTrue(("Buzz\n").equals(game.printInRange(52, 52, true)));
+	}
+	
+	@Test
+	public void testPrintInRange_stage2_04() {
+		assertTrue(("FizzBuzz\n").equals(game.printInRange(51, 51, true)));
+	}
+
+	@Test
+	public void testPrintInRange_stage2_05() {
+		assertTrue(("Buzz\n").equals(game.printInRange(5, 5, true)));
+	}
+
+	@Test
+	public void testPrintInRange_stage2_06() {
+		assertTrue(("FizzBuzz\n").equals(game.printInRange(15, 15, true)));
+	}
+	
+	@Test
+	public void testPrintInRange_stage2_07() {
+		assertTrue(("1\n" + 
+				"2\n" + 
+				"Fizz\n" + 
+				"4\n" + 
+				"Buzz\n" + 
+				"Fizz\n" + 
+				"7\n" + 
+				"8\n" + 
+				"Fizz\n" + 
+				"Buzz\n" + 
+				"11\n" + 
+				"Fizz\n" + 
+				"Fizz\n" + 
+				"14\n" + 
+				"FizzBuzz\n" + 
+				"16\n").equals(game.printInRange(1, 16, true)));
+	}
+	
+	@Test
+	public void testPrintInRange_stage2_08() {
+		assertTrue(("49\n" + 
+				"Buzz\n" + 
+				"FizzBuzz\n" + 
+				"Buzz\n" + 
+				"FizzBuzz\n" + 
+				"FizzBuzz\n" + 
+				"Buzz\n" + 
+				"Buzz\n" + 
+				"FizzBuzz\n" + 
+				"Buzz\n" + 
+				"Buzz\n" + 
+				"FizzBuzz\n" + 
+				"61\n").equals(game.printInRange(49, 61, true)));
+	}
 }
